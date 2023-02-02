fa7f3c4d350b5164b0a5edc3e6bd7a5453295cd7
TomoProg <helloworld0306.xxx@gmail.com>
2023-02-02 09:05:19 +0900

    part2 fin.

--- db.c
+++ db.c
@@ -13,6 +13,29 @@ typedef struct {
 	ssize_t input_length; // ssize_tって何やっけ？
 } InputBuffer;
 
+// メタコマンドの解析結果
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+// SQL文の解析結果
+typedef enum {
+	PREPARE_SUCCESS,
+	PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+// SQL文の種別
+typedef enum {
+	STATEMENT_INSERT,
+	STATEMENT_SELECT
+} StatementType;
+
+// SQL文の構造
+typedef struct {
+	StatementType type;
+} Statement;
+
 InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
 	input_buffer->buffer = NULL;
@@ -53,17 +76,70 @@ void close_input_buffer(InputBuffer* input_buffer) {
 	free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+	if(strcmp(input_buffer->buffer, ".exit") == 0) {
+		exit(EXIT_FAILURE);
+	} else {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+	// insertの場合は後ろにデータが続くため、strncmpを使っている
+	// (e.g. insert 1 cstack foo@bar.com)
+	// selectも同じなのでは？
+	if(strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	if(strcmp(input_buffer->buffer, "select") == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;
+	}
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+	switch(statement->type) {
+		case(STATEMENT_INSERT):
+			printf("This is where we would do an insert\n");
+			break;
+		case(STATEMENT_SELECT):
+			printf("This is where we would do an select\n");
+			break;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	InputBuffer* input_buffer = new_input_buffer();
 	while(true) {
 		print_prompt();
 		read_input(input_buffer);
 
-		if(strcmp(input_buffer->buffer, ".exit") == 0) {
-			close_input_buffer(input_buffer);
-			exit(EXIT_SUCCESS);
-		} else {
-			printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+		// SQLiteのメタコマンドかどうかを確認する
+		// 先頭が"."であれば、メタコマンド
+		if(input_buffer->buffer[0] == '.') {
+			switch(do_meta_command(input_buffer)) {
+				case(META_COMMAND_SUCCESS):
+					continue;
+				case(META_COMMAND_UNRECOGNIZED_COMMAND):
+					printf("Unrecognized command '%s'\n", input_buffer->buffer);
+					continue;
+			}
 		}
+
+		// 入力されたSQLを解析する
+		Statement statement;
+		switch(prepare_statement(input_buffer, &statement)) {
+			case(PREPARE_SUCCESS):
+				break;
+			case(PREPARE_UNRECOGNIZED_STATEMENT):
+				printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+				continue;
+		}
+
+		// SQL実行
+		execute_statement(&statement);
+		printf("Executed.\n");
 	}
 }
